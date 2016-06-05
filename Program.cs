411c897d9d0f291e283aabd65d6801507e0626bd
Oriolowo Nancy <oriolowonancy@gmail.com>
2016-06-06 00:47:30 +0100

    nancee commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practisingrandomlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int mylist;
+            Random generator = new Random();
+           
+            for (int i = 0; i < 10; i++ )
+            {
+                mylist = Convert.ToInt32(generator.NextDouble() * 20);
+                Console.WriteLine("{0}",mylist);
+            }
+
+                Console.ReadLine();
+        }
+    }
+}
